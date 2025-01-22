deffa9d8f990972ef5a106de07dab6c9dfb8189c
m4p.ednaldo.dilorenzo@polodeinovacao.ifpb.edu.br <m4p.ednaldo.dilorenzo@polodeinovacao.ifpb.edu.br>
2025-01-22 14:53:00 -0300

    Changes to enable the budget update.

--- server/model/budget.model.go
+++ server/model/budget.model.go
@@ -2,9 +2,9 @@ package model
 
 type Budget struct {
 	UserDependent
-	Year       uint  `gorm:"primay_key" json:"year"`
-	CategoryId uint8 `gorm:"primay_key" json:"categoryId"`
-	Value      int32 `gorm:"type:int(10)" json:"value"`
+	Year       uint  `gorm:"type:int" json:"year"`
+	CategoryId uint8 `gorm:"type:int" json:"categoryId"`
+	Value      int64 `gorm:"type:int(10)" json:"value"`
 }
 
 func (Budget) TableName() string {
